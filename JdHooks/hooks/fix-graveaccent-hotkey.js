30d2ee8c8032d5631f6667a89d767dc01ddb2442
EvilSpark <kamrantahir25@gmail.com>
2020-04-27 13:20:02 +0530

    :fire: Removed the files which can be covered using AutoMod
    Removed the files which can be covered using AutoMod

--- JdHooks/hooks/fix-graveaccent-hotkey.js
+++
@@ -1,8 +0,0 @@
-//Hotkeys: "`/~" button works again. May work incorrectly with some keyboards.
-//Горячие клавиши: кнопка "`/~" снова работает. Может работать неправильно с некоторыми клавиатурами.
-
-vivaldi.jdhooks.hookModule("_getPrintableKeyName", function(moduleInfo, exportsInfo) {
-
-    exportsInfo.exports[192] = '`';
-
-});
--- ModContainer/hooks/fix-graveaccent-hotkey.js
+++
@@ -1,4 +0,0 @@
-//Hotkeys: "`/~" button works again. May work incorrectly with some keyboards.
-//Горячие клавиши: кнопка "`/~" снова работает. Может работать неправильно с некоторыми клавиатурами.
-
-vivaldi.jdhooks.hookModule("_getPrintableKeyName", (moduleInfo, exports) => { return { ...exports, ...{ 192: '`' } } })
